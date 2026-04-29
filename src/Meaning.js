8e2a08e537ad58f56934cbd4902459c06759aa41
Gire1 <168452060+Gire1@users.noreply.github.com>
2026-04-29 09:53:30 +0200

    Added the Search Engine

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,25 +1,21 @@
-import React from "react"
+import React from "react";
 
-
-export default function Meaning(props){
-    return (<div className="Meaning">
-       <h3>{props.meaning.partOfSpeech}</h3> 
-        {props.meaning.defination.map(funtion(definition,index)
-
-                 return (
-            <div key={index}>
-                <p>
-                    {definition.definition}
-                    < br />
-                    <em>{definition.example}</em>
-                </p>
-            </div>
-                 );
-    })}
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
-    );
-    }   
-
-          
-
-    
+  );
+}
